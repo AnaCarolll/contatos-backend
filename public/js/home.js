443a2baf1fa2de8cc030475909fce07748796319
Ana Carol <progamacaocarol2021@gmail.com>
2022-09-01 17:36:30 -0300

    entendemos o addEventListener e preventDefacult

--- public/js/home.js
+++ public/js/home.js
@@ -1,147 +1,52 @@
-const loginContainer = document.getElementById("login-container");
-const appContainer = document.getElementById("app-container");
-const formLogin = document.querySelector('#login-container > form');
-const emailLogin = document.querySelector('#email');
-const senhaLogin = document.querySelector('#senha');
-const main = document.querySelector('main');
-const modal = document.getElementById("modal");
-const link = document.getElementById("linkAbrirModal");
-const edits = document.querySelectorAll("section > a");
-const search = document.getElementById('search');
-const cancelButton = document.querySelector('#modal button.link');
-const inputParaFocus = document.querySelector('#modal input[name="nome"]')
 
-const mostrarModal = () => {
-    modal.style.display = "flex";
-    modal.style.opacity = 1;
-    inputParaFocus.focus()
-};
+const emailEsperado = "eu@gmail.com.br";
+const senhaEsperada = "12345";
+//capturar todos os elemestos de interesse na página
+const form = document.getElementById("form");
 
-const esconderModal = (e) => {
-    e.bubbles = false;
-    modal.style.display = "none"
-    modal.style.opacity = 0;
-};
+const inputEmail = document.querySelector("#form input[type=email]");
+inputEmail.value = emailEsperado;
 
-const showContatos = contatos => {
-    main.innerHTML = '';
-    contatos.forEach(c => {
-        const section = document.createElement('section');
+const inputSenha = document.querySelector("#form input[type=password]");
+inputSenha.style.backgraudcolor = "red"
 
-        let htmlEmails = '';
-        c.emails.forEach(e => {
-            htmlEmails += `<a href="mailto:${e}">${e}</a>`;
-        });
+const buttonEntrar = document.querySelector("#form button[type=submit]");
+buttonEntrar.innerHTML = "start :)";
 
-        let htmlTels = '';
-        c.telefones.forEach(t => {
-            htmlTels += `<li><a href="tel:${t}">${t}</a></li>`;
-        });
-
-        const html = `
-            <h3>${c.nome}</h3>
-            <div>
-                ${htmlEmails}
-            </div>
-            <ul>
-                ${htmlTels}
-            </ul>
-            <a href="#">Editar</a>
-        `;
-
-        section.innerHTML = html;
-
-        main.appendChild(section);
-    });
-};
-
-const buscaContatos = trecho => {
-    const contatosFiltrados = contatos.filter(
-        c => c.nome.toUpperCase().includes(trecho.toUpperCase())
-    );
-
-    showContatos(contatosFiltrados);
-};
-
-const carregaContatos =  async ()=> {
-
-    // Carregando token do sessionStorage
-    let token = sessionStorage.getItem('token');
-
-    let resposta = await fetch(
-        '/contatos',
-        {
-            method: "GET",
-            headers: {
-                authorization: `bearer ${token}`
-            }
-        }
-    );
-    let contatos = await resposta.json();
-    showContatos(contatos);
-
-}
-
-const login = async dadosDeLogin =>{
-    
-    let response = await fetch(
-        '/login',
-        {
-            method:"POST",
-            body: JSON.stringify(dadosDeLogin),
-            headers: {
-                "content-type":"application/json"
-            }
-        }
-    );
-
-    // Verificando se o login obteve sucesso...
-    if(response.status == 403){
-        
-        alert("Login Inválido");
-        return;
-
-    } else if(response.status == 200){
-
-        // Acessar o conteúdo da reponse
-        let dados = await response.json();
-
-        // Salvar o token
-        sessionStorage.setItem('token', dados.token);
-
-        // Mostrar o app container container e esconder o login;
-        appContainer.style.display = "block";
-        loginContainer.style.display = "none";
-
-        // Carregar os contatos
-        carregaContatos();
-
-    } else {
-
-        alert(`Erro inesperado. Entre em contato com o suporte.\n${response.statusText}`);
-
-    }
-
-}
-
-search.addEventListener('keyup', (e) => buscaContatos(e.target.value));
-link.addEventListener('click', mostrarModal);
-cancelButton.addEventListener('click', esconderModal);
-modal.addEventListener('keyup', e => e.key === 'Escape' ? esconderModal(e) : null);
-formLogin.addEventListener(
+form.addEventListener(
     'submit',
-    e => {
-        // Interromper o comportamento padrão do evento;
-        e.preventDefault();
-        
-        // Ler os dados de login
-        let dadosDeLogin = {
-            email: emailLogin.value,
-            senha: senhaLogin.value
+    (evento) => {
+        // inibir o envio do formulario
+        evento.preventDefault()
+
+        //Verificar se o email digitado é igual ao emailEsperado
+        if(inputEmail.value != emailEsperado){
+            alert("Falha no login");
+            return
         }
+        // Verificar se a senha digitada é igual senhaEsperada
 
-        // Chamar uma função para fazer o login
-        login(dadosDeLogin);
-
+        //Se estiver ok, emitir mensagem de sucesso
+        alert("login deu certo!")
     }
 );
+let link = document.getElementById("link");
+link.addEventListener(
+  'click',
+  evt => {
+    
+  }
+)
+
+//adicionar um escutador de evento
+//inputEmail.addEventListener(
+  //  'blur',
+    //() => {
+  //if(inputSenha.value == "" || inputEmail.value == "")
+  //alert("Ops, campo vazio, \npreencha-o!")
+    //});
+
+//#label{
+ ///   boder: solid 3px pink;
+//};
+//input.style.color = "black"
